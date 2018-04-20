63cdea518a21ad704f83fe50c339a2f7d2d401e4
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-20 18:26:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:username]
+      session[:username] = params[:username]
+    else
+      redirect to '/new'
+    end 
   end
 
   def destroy
-    session.destroy :username 
+    session.destroy :username
   end
 end
